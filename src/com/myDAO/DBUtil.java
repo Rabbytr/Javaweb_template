a458e3547f73d10cb3c9a54c0b341acc15bac6c4
Rabbytr <972592943@qq.com>
2018-05-10 01:26:54 +0800

    add DBUtil.java

--- src/com/myDAO/DBUtil.java
+++ src/com/myDAO/DBUtil.java
@@ -1,5 +1,40 @@
 package com.myDAO;
 
-public class DBUtil {
+import static java.lang.Class.forName;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBUtil {
+	private static String driver;
+	private static String url;
+	private static String username;
+	private static String password;
+	
+	static {
+		driver = "";
+		url = "";
+		username = "";
+		password = "";
+	}
+	
+	public static Connection open() {
+		try {
+			forName(driver);
+			return DriverManager.getConnection(url, username, password);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static void close(Connection conn) {
+		 if(conn!=null) {
+			 try {
+				conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		 }
+	}
 }
